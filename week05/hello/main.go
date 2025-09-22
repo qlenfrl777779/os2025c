b8c18113ce51573d1f94ac5a80f2db34ff48e9df
rnrygus0132 <rnrygus0132@gmail.com>
2025-09-22 18:22:38 +0900

    v0.2 hello/main.go & week05.txt update

--- week05/hello/main.go
+++ week05/hello/main.go
@@ -1,8 +1,12 @@
-package main
-
+package main // main pacakge
 import "fmt"
 
-func main() {
-	name := "Go"
-	fmt.Println("Hello", name)
+// fmt package
+
+func main() { // 파이썬 : def, 고 : func
+	// name := "Go"
+	// fmt.Println("Hello", name) // 인수 여러개 가능
+
+	number := 7
+	fmt.Printf("Number is %d.", number)
 }
