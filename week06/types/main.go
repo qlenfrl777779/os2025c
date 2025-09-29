8c53db985a39032ad86c41bd6e3d3a41555f4d7a
rnrygus0132 <rnrygus0132@gmail.com>
2025-09-29 17:07:55 +0900

    v0.2 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -10,4 +10,6 @@ func main() {
 	fmt.Println(math.Floor(2.21))
 	fmt.Println(math.Ceil(2.21))
 	fmt.Println(strings.Title("go developer!"))
+	fmt.Println("kim\nInha\t\"\\") // \n :줄바꿈 \t: tap만큼 띄우기 \": 따옴표 출력 \\: 역슬래시 출력
+	fmt.Println('2', '가', '\u18a0')
 }
