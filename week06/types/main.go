9ba41e00b5ab2e5c22416f8c8be627b32cb39302
rnrygus0132 <rnrygus0132@gmail.com>
2025-09-29 17:13:40 +0900

    v0.3 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strings"
+	"reflect"
 )
 
 func main() {
-	fmt.Println(math.Floor(2.21))
-	fmt.Println(math.Ceil(2.21))
-	fmt.Println(strings.Title("go developer!"))
-	fmt.Println("kim\nInha\t\"\\") // \n :줄바꿈 \t: tap만큼 띄우기 \": 따옴표 출력 \\: 역슬래시 출력
-	fmt.Println('2', '가', '\u18a0')
+	fmt.Println(reflect.TypeOf(2.31))
+	fmt.Println(reflect.TypeOf("Kim Inha"))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf('A'))
+	fmt.Println(reflect.TypeOf(19))
 }
