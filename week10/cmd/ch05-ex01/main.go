3ed12d76da30c9bcbd1a96e124a8bec0463492fa
rnrygus0132 <rnrygus0132@gmail.com>
2025-11-03 18:23:00 +0900

    week10 array v0.5

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -5,11 +5,14 @@ import (
 )
 
 func main() {
-	arrayBool := [2]bool{true, false} // array literal
-	arrayInt := [3]int{-9, 11, 7}
-	for i := 0; i < len(arrayInt); i++ {
-		fmt.Println(i, arrayBool[i])
-		fmt.Println(i, arrayInt[i])
+	// 	numbers := [3]int{-9, 11, 7}
+	// 	for i := 0; i < len(numbers); i++ {
+	// 		fmt.Println(i, numbers[i])
+	// 	}
+
+	numbers := [3]int{-9, 11, 7}
+	for i, number := range numbers {
+		fmt.Println(i, number)
 	}
 
 }
