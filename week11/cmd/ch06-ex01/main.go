b1c78bd42fdeb250a580420d46d303e3bf7d32c6
rnrygus0132 <rnrygus0132@gmail.com>
2025-11-10 17:38:42 +0900

    week11 slice v0.5

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	subjects := [4]string{"Go", "Javascript", "Python", "Linux"}
-	subjectSlice := subjects[1:3] // slicing
+	subjectSlice := subjects[:3] // slicing
+	// subjects[0] = "Java"
+	subjectSlice[0] = "Database"
 	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
