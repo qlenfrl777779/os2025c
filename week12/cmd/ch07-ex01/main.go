292daf6db2f76c420ce9f90ec5cbcfb487e4687d
rnrygus0132 <rnrygus0132@gmail.com>
2025-11-17 17:08:50 +0900

    week12 slice v0.95

--- week12/cmd/ch07-ex01/main.go
+++ week12/cmd/ch07-ex01/main.go
@@ -12,5 +12,24 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(lines)
+	// fmt.Println(lines)
+	var names []string
+	var counts []int
+	for _, line := range lines {
+		matched := false
+		for i, name := range names {
+			if name == line {
+				counts[i]++
+				matched = true
+			}
+		}
+		if matched == false { // 처음 나온 이름
+			names = append(names, line)
+			counts = append(counts, 1)
+		}
+	}
+
+	for i, name := range names {
+		fmt.Println(name, ":", counts[i])
+	}
 }
