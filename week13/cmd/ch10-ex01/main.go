9953325135cefee1797f1fde4e2fb09e6d9d2be6
rnrygus0132 <rnrygus0132@gmail.com>
2025-11-24 18:11:14 +0900

    week13 encapsulation v0.5

--- week13/cmd/ch10-ex01/main.go
+++ week13/cmd/ch10-ex01/main.go
@@ -11,5 +11,6 @@ func main() {
 	today.SetYear(2025)
 	today.SetMonth(11)
 	today.SetDay(24)
-	fmt.Println(today.Year(), "년", today.Month(), "월", today.Day(), "일")
+	// fmt.Println(today.Year(), "년", today.Month(), "월", today.Day(), "일")
+	fmt.Printf("%d년 %d월 %d일\n", today.Year(), today.Month(), today.Day())
 }
