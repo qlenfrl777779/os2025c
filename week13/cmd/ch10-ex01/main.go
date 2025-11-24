9738511a27a3eb2044288a001e8b1471f325fc87
rnrygus0132 <rnrygus0132@gmail.com>
2025-11-24 18:10:01 +0900

    week13 encapsulation

---
+++ week13/cmd/ch10-ex01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"week13/pkg/calendar"
+)
+
+func main() {
+	today := calendar.Date{}
+	// today.year = 2025 캡슐화되어 보이지않음
+	today.SetYear(2025)
+	today.SetMonth(11)
+	today.SetDay(24)
+	fmt.Println(today.Year(), "년", today.Month(), "월", today.Day(), "일")
+}
