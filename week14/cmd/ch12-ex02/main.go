87cc3da1d964d65b3263bad58c2036aae9e8471a
rnrygus0132 <rnrygus0132@gmail.com>
2025-12-01 17:58:35 +0900

    week14 panic v0.2

--- week14/cmd/ch12-ex02/main.go
+++ week14/cmd/ch12-ex02/main.go
@@ -2,9 +2,30 @@ package main
 
 import "fmt"
 
+func safeDivide(a, b int) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("에러 발생:", err)
+		}
+	}()
+
+	if b == 0 {
+		panic("0으로 나눌 수 없습니다!")
+	}
+
+	result := a / b
+	fmt.Println("결과:", result)
+}
+
 func main() {
-	defer fmt.Println("프로그램 종료 - 정리작업")
 	fmt.Println("1. 프로그램 시작")
 	panic("심각한 에러 발생!")
 	fmt.Println("2. 이 줄은 실행되지 않음")
+	fmt.Println("첫 번째 호출")
+	safeDivide(10, 2)
+
+	fmt.Println("\n두 번째 호출")
+	safeDivide(10, 0)
+
+	fmt.Println("\n프로그램 계속 실행됨")
 }
